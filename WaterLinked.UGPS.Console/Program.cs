57621fad8cb1c6255ffef0d7ec11134448bcbd52
Jonas Follesø <jonas@follesoe.no>
2022-09-07 13:49:25 +0200

    Add status to example console

--- WaterLinked.UGPS.Console/Program.cs
+++ WaterLinked.UGPS.Console/Program.cs
@@ -8,3 +8,9 @@ var about = await client.ApiV1AboutAsync();
 Console.WriteLine($"Product name: {about.Product_name}");
 Console.WriteLine($"Product id: {about.Product_id}");
 Console.WriteLine($"Hardware revision: {about.Hardware_revision}");
+
+var statusReport = await client.ApiV1StatusReportAsync();
+foreach (var report in statusReport)
+{
+    Console.WriteLine($"{report.Status_group_id} {report.Status}: {report.Message}");
+}
